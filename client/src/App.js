dd13fe4a58e49de74ad78614641c242d38feb33c
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-01 10:47:34 -0400

    added material ui npm. got rid of unused code and files. working on setting up file structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Contacts} />
+        <Route exact path="/add" component={AddContact} />
+        <Route exact path="/edit/:id" component={EditContact} />
+        <Route exact path="/contacts/:id" component={Detail} />
+      </Switch>
+    </Router>
   );
 }
 
