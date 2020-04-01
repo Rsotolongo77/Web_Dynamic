d1006dec57c1f793186335381d08f2db626766bf
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-01 11:01:17 -0400

    set up basic file structure in regards to pages, and app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Home from '../src/components/Home';
+import About from '../src/components/About';
+import Contact from '../src/components/Contact';
+import Portfolio from '../src/components/Portfolio';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Contacts} />
-        <Route exact path="/add" component={AddContact} />
-        <Route exact path="/edit/:id" component={EditContact} />
-        <Route exact path="/contacts/:id" component={Detail} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
       </Switch>
     </Router>
   );
