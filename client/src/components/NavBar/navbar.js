19ed21b567b50031564fee938774cbf683375c46
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-11 15:26:13 -0400

    fixed spacing on navBar elements.

--- client/src/components/NavBar/navbar.js
+++ client/src/components/NavBar/navbar.js
@@ -5,17 +5,31 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid';
+import white_logo_transparent from '../../images/white_logo_transparent.png';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(4)
+    },
+    menuIcon: {
+        fontSize: '4.5rem',
+    },
+    logo: {
+        height: '12rem',
+        marginLeft: theme.spacing(7),
+        marginTop: theme.spacing(1)
     },
     title: {
         flexGrow: 1,
     },
+    navColor: {
+        backgroundColor: 'transparent',
+        boxShadow: 'none'
+    }
 }));
 
 const NavBar = () => {
@@ -23,15 +37,18 @@ const NavBar = () => {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.navColor}>
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-          </Typography>
-
+                    <Grid container>
+                        <Grid item xs={10}>
+                            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                                <MenuIcon className={classes.menuIcon} />
+                            </IconButton>
+                        </Grid>
+                        <Grid container item xs={2} justify='center'>
+                            <img className={classes.logo} src={white_logo_transparent} />
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </div>
