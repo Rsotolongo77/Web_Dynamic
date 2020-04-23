392ae39d75fd6a67ee89901003a01d65cfe338d5
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-04-23 09:32:32 -0400

    started aboutme component for aboutme page.

---
+++ client/src/components/MainAbout/mainAbout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './mainAbout.css';
+
+
+const MainAbout = () => {
+    return (
+        <div className='mainAbout'>
+
+        </div>
+    );
+}
+
+export default MainAbout;
