d51faa98aef1249bdd3254ad1b2e1f6e0542ab7c
Ricardo Sotolongo <rickey.soto@gmail.com>
2020-06-06 17:55:57 -0400

    started contact component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home/home';
 import About from './pages/About/about';
-// import Contact from '../pages/Contact/contact';
+import Contact from '../pages/Contact/contact';
 // import Portfolio from '../pages/Portfolio/portfolio';
 import './App.css';
 
@@ -12,8 +12,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        {/* <Route exact path="/contact" component={Contact} />
-        <Route exact path="/portfolio" component={Portfolio} /> */}
+        <Route exact path="/contact" component={Contact} />
+        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
       </Switch>
     </Router>
   );
